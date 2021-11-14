08da42a2a1987cc1465b19080d47d3dd1bc05894
Jorge Sanguinetti <sanguinettixd@gmail.com>
2021-11-14 16:59:47 -0600

    first commit

---
+++ database/migrations/2021_11_14_132734_create_calificacions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCalificacionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('calificaciones', function (Blueprint $table) {
+            $table->id();
+            $table->int('puntuacion');
+            $table->string('comentario')->nullable();
+            $table->unsignedBigInteger('usuario_id');
+            $table->foreign('usuario_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('calificacions');
+    }
+}
